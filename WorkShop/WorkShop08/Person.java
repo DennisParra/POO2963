81bc416289adb7deef0c292ee76434351f9de057
DennisParra <dennisparra1999@gmail.com>
2019-10-17 11:34:20 -0500

    version 0.15

---
+++ WorkShop/WorkShop08/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    private int num;
+    private String str;
+
+    public Person() {
+
+    }
+
+    public Person(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
