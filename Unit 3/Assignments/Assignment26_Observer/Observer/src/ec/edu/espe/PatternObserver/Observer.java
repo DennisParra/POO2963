8118dd9ef9478d0976046f5dfc48fc4b180168b2
DennisParra <dennisparra1999@gmail.com>
2020-01-23 22:22:31 -0500

    Version 3.7

---
+++ Unit 3/Assignments/Assignment26_Observer/Observer/src/ec/edu/espe/PatternObserver/Observer.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.PatternObserver;
+
+/**
+ *
+ * @author HP
+ */
+public interface Observer {
+	
+	//method to update the observer, used by subject
+	public void update();
+	
+	//attach with subject to observe
+	public void setSubject(Subject sub);
+}
