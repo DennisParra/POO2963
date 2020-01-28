7b515cf91b13e891fd83c76f27934ae3d3dda091
DennisParra <dennisparra1999@gmail.com>
2020-01-28 10:29:12 -0500

    Edicion

--- Unit 3/Project/FastPay/src/ec/edu/espe/FastPay/view/FastPay.java
+++ Unit 3/Project/FastPay/src/ec/edu/espe/FastPay/view/FastPay.java
@@ -1,54 +1,33 @@
-//package ec.edu.espe.FastPay.view;
-//
-//import ec.edu.espe.FastPay.controller.Entry;
-//import java.util.Scanner;
-//
-//public class FastPay {
-//    public static void main(String[] args) {
-//        
-//        Menu();
-//        
-//    }
-//        public static void Menu(){
-//        
-//            Entry entry = new Entry();
-//
-//            Scanner enter = new Scanner(System.in);
-//
-//            int option;
-//            char respuesta;
-//
-//            do{
-//                System.out.println("1. Enter");
-//                System.out.println("2. Modify");
-//                System.out.println("3. Delete");
-//                System.out.println("4. See");
-//                System.out.println("5. Exit");
-//
-//                System.out.println("Please input an option: ");
-//                option = enter.nextInt();
-//                switch(option){
-//                    case 1:
-//                        entry.enter();
-//                        break;
-//                    case 2: 
-//                        //modify
-//                        break;
-//                    case 3:
-//                        //Delete
-//                        break;
-//                    case 4:
-//                        //see
-//                        break;
-//                    case 5: 
-//                        System.out.println("Thanks for use our system\n");
-//                        System.out.println("Bye :)");
-//                        break;
-//                    default:
-//                        System.out.println("Invalid option, input again");
-//                        break;
-//                }
-//            }while(option !=5);
-//        }
-//    }
-//
+/*package ec.edu.espe.FastPay.view;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import ec.edu.espe.FastPay.controller.Entry;
+import java.util.List;
+import java.util.Scanner;
+
+public class FastPay {
+
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        //Connecting With Server Please add the external jar file first
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        System.out.println("server connection successfully done");
+
+        //Connecting with database
+        MongoDatabase dbs = mongoClient.getDatabase("test");
+        System.out.println("Connect to database successfully");
+        System.out.println("Database Name" + dbs.getName());
+
+        // To get all database names
+        List<String> dbNames = mongoClient.getDatabaseNames();
+        System.out.println(dbNames);
+
+        //Drop Database
+        mongoClient.dropDatabase("newDb");
+        dbNames = mongoClient.getDatabaseNames();
+        System.out.println(dbNames);
+    }
+
+}*/
